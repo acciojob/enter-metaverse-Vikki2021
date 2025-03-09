e4985d3a349fffdea777a8d7144b1b8ebd22f471
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-09 17:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let status = document.getElementById("status")
 let enterBtn = document.getElementById("enterBtn")
 
-function enter() {
-	status.innerHTML = `<h1>Enter the Metaverse</h1>`
-}
+enterBtn.addEventListener("click", ()=> {
+  status.innerHTML = `<h1>Enter the Metaverse</h1>`
+})
+
+
