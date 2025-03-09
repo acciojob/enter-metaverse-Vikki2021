fa7d9a0ec17a586f81767aa9acb88daca927105d
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-09 17:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let status = document.getElementById("status")
+let enterBtn = document.getElementById("enterBtn")
+
+function enter() {
+	status.innerHTML = `<h1>Enter the Metaverse</h1>`
+}
